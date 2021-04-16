a934f0cb48d3475664dcb2f63da0817250fc9773
KarmaZA <everatt.jonathon@gmail.com>
2021-04-16 09:31:52 +0200

    Added the outline for the answer to Q2 but its not working yet. Though I'm sure this is the way to answer it

--- partA-question2.hs
+++ partA-question2.hs
@@ -1,9 +1,10 @@
-data Suit = Hearts | Clubs | Diamonds | Spades
-      deriving eq
-data Rank = Numeric Int | King | Queen | Jack | Ace
-      deriving eq
-data Card = NormalCard Rank Suit | Joker
-      deriving eq
+data Suit = Hearts | Clubs | Diamonds | Spades deriving Eq
+data Rank = Numeric Int | King | Queen | Jack | Ace deriving Eq
+data Card = NormalCard Rank Suit | Joker deriving Eq
 
-
+--Essentially this is the way I think the answer should be structured there is a pattern error in the (_ _) line
 countAces :: [Card] -> Int
+countAces Joker              = 1 --if the card if a joker
+countAces NormalCard (Ace _) = 1 --elif ace +1
+countAces NormalCard (_ _)   = 0 -- else plus 0
+countAces (x:xs) = 0 + countAces x + countAces xs
