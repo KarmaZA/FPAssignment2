d9c8f44a02081d581717ad3b0488fd2885807f8f
KarmaZA <everatt.jonathon@gmail.com>
2021-04-28 22:24:49 +0200

    Worked out most of A1 just need to get one more error down and it should be working. TOo tired to look it up rn

--- partA-question1.hs
+++ partA-question1.hs
@@ -1,14 +1,19 @@
 --Define a more general function gf that uses bf,df as such that
 --propgf xs == bs xs == gf abs xs
 --	&& df xs == gf (+1) xs
-bf,df :: [Int] -> [Int]
-bf [] = []
-bf [x] = [abs x]
-bf (x:y::xs) = (abs x) : y : bf xs
+--bf,df :: [Int] -> [Int]
+--bf [] = []
+--bf [x] = [abs x]
+--bf (x:y::xs) = (abs x) : y : bf xs
 
-df [] = []
-df [x] = [x+1]
-df (x:y:xs) = (x+1) : y : df xs
+--df [] = []
+--df [x] = [x+1]
+--df (x:y:xs) = (x+1) : y : df xs
 
 
 gf :: (a -> a) -> [a] -> [a]
+gf _ [] = []
+gf abs [x] = [abs x]
+gf abs (x:y:xs) = (abs x) : y : gf abs xs
+gf f [x] = [x+1]
+gf f (x:y:xs) = (x+1) : y : gf f xs
