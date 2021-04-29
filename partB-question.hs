6b281cdef10fee2a1f3a7c2d0811a5f1467c505c
KarmaZA <EVRJON003@myuct.ac.za>
2021-04-29 20:20:44 +0200

    Update partB-question.hs
    Should be working now for any palindrome starting on a

--- partB-question.hs
+++ partB-question.hs
@@ -11,5 +11,25 @@ moveRight (Tape xs y (z:zs)) = Tape (y:xs) z zs
 --Operation to write the symbol on the tape
 write (Tape xs y zs) a = Tape xs a zs
 
---implementation of the Turing machine
-Palin:: Int -> Tape Char -> Maybe(Int, Tape Char)
+blanks = repeat ' '
+--implementation of the Turing machinie
+palin:: Int -> Tape Char -> Maybe(Int, Tape Char)
+palin s t@(Tape _ a _) = 
+   case (s,a) of
+      (0,'a')  -> Just (10, moveRight (write t '_'))  -- first item a
+      (0,'b')  -> Just (20, moveRight (write t '_'))  -- First item b
+      (0,' ')  -> Just (30, write t 'Y')  --nothin
+      (10,'a') -> Just (10, moveRight t) 
+      (10,'b') -> Just (10, moveRight t)
+      (10,' ') -> Just (11, moveLeft t)
+      (11,'a') -> Just (12, moveLeft (write t ' '))
+      (11,'b') -> Just (30, write t 'N')   -- start a end b reject
+      (11,' ') -> Just (30, write t 'Y')   -- only a accept
+      (12,'a') -> Just (12, moveLeft t)
+      (12,'b') -> Just (12, moveLeft t)
+      (12,' ') -> Just (0, moveRight t)
+      (30,'Y') -> Just (30, write t 'a')
+      (30,'N') -> Just (30, write t 'b')
+      (30,'a') -> Nothing
+      (30,'b') -> Nothing
+      _        -> error (show a)
