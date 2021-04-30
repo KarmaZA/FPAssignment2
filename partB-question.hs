26912d07de78d9bce2539002556582649bced856
KarmaZA <EVRJON003@myuct.ac.za>
2021-04-30 11:21:02 +0200

    Turing machine working

--- partB-question.hs
+++ partB-question.hs
@@ -1,5 +1,8 @@
 --Implementation of a Turing machine for a palindrome of length 4 or less
---
+-- The syntax to running this Turing machine is:
+-- showT (runTM (tmPal (Tape blanks 'a' ("bbb" ++ blanks))))
+-- where a is the first char on the tape and bbb is the rest of the tape. if only 1 char use (Tape blanks 'a' blanks)
+
 
 --Representation of input tape for a Turing Machine
 data Tape a = Tape [a] a [a]
@@ -11,13 +14,28 @@ moveRight (Tape xs y (z:zs)) = Tape (y:xs) z zs
 --Operation to write the symbol on the tape
 write (Tape xs y zs) a = Tape xs a zs
 
+data TM a s = TM [a] 
+                 [s] 
+                 s 
+                 (Tape a) 
+                 (s -> Tape a -> Maybe (s, Tape a))
+
+runTM :: TM a s -> Tape a
+runTM (TM as ss s0 t0 tf) =
+  case tf s0 t0 of
+      Nothing       -> t0
+      Just (s1,t1) -> runTM (TM as ss s1 t1 tf)
+
+showT (Tape xs y zs) =
+             y
+
 blanks = repeat ' '
 --implementation of the Turing machinie
 palin:: Int -> Tape Char -> Maybe(Int, Tape Char)
 palin s t@(Tape _ a _) = 
    case (s,a) of
-      (0,'a')  -> Just (10, moveRight (write t '_'))  -- first item a
-      (0,'b')  -> Just (20, moveRight (write t '_'))  -- First item b
+      (0,'a')  -> Just (10, moveRight (write t ' '))  -- first item a
+      (0,'b')  -> Just (20, moveRight (write t ' '))  -- First item b
       (0,' ')  -> Just (30, write t 'Y')  --nothin
       (10,'a') -> Just (10, moveRight t) 
       (10,'b') -> Just (10, moveRight t)
@@ -28,8 +46,27 @@ palin s t@(Tape _ a _) =
       (12,'a') -> Just (12, moveLeft t)
       (12,'b') -> Just (12, moveLeft t)
       (12,' ') -> Just (0, moveRight t)
+      (20,'a') -> Just (20, moveRight t)
+      (20,'b') -> Just (20, moveRight t)
+      (20,' ') -> Just (21, moveLeft t)
+      (21,'b') -> Just (22, moveLeft (write t ' '))
+      (21,'a') -> Just (30, write t 'N')   -- start a end b reject
+      (21,' ') -> Just (30, write t 'Y')   -- only a accept
+      (22,'b') -> Just (22, moveLeft t)
+      (22,'a') -> Just (22, moveLeft t)
+      (22,' ') -> Just (0, moveRight t)
       (30,'Y') -> Just (30, write t 'a')
       (30,'N') -> Just (30, write t 'b')
       (30,'a') -> Nothing
       (30,'b') -> Nothing
-      _        -> error (show a)
+      _        -> error (show s ++ show a)
+
+--tPal1, tPal2, tPal3, tPal4 :: Tape Char
+--tPal1 = Tape blanks 'a' ("bba" ++ blanks)
+--tPal2 = Tape blanks 'b' ("aabaab" ++ blanks)
+--tPal3 = Tape blanks ' ' blanks
+--tPal4 = Tape blanks 'b' blanks
+--tPal5 = Tape blanks 'a' ("bab" ++ blanks)
+
+tmPal :: Tape Char -> TM Char Int
+tmPal t = TM " abYN" [0,10,11,12,20,21,22,30] 0 t palin
