581af359bcb2dac94d549bc1cb388fc4ef986b24
KarmaZA <EVRJON003@myuct.ac.za>
2021-04-30 11:53:07 +0200

    Submission files
    The final iteration on the turing machine and read me as well as the .zip file I used to submit my project downloaded from vula

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-This text file contains the sequence of possible Turing machine state configurations that are used during the computation of g with the input string of "aba" 
+This text file contains the sequence of possible Turing machine state configurations that are used during the computation of g with the input string of "aba"
 
 List of configurations given as
 <Current State, Input Symbol, Output Symbol, New State>
@@ -12,12 +12,8 @@ The input string on the tape can be read as " aba ". For readability sake I will
 <S10, 'a', 'a', S10>
 <S10, ' ', ' ', S11>
 <S11, 'a', ' ', S12>
-<S12, 'b', 'b', S12>
-<S12, ' ', ' ', S0>
-<S0 , 'b', ' ', S20>
-<S20, ' ', ' ', S21>
-<S21, ' ', 'Y', S30>
-<S30, 'Y', 'a', S30>
+<S12, 'b', ' ', S16>
+<S16, ' ', 'a', S0>
 <S30, 'a', 'a', Halt>
-End of the program will show the tape which will read 'a' with a number of
- spaces on either side.
+'a'
+End of the program will show the tape which will read 'a' with a number of spaces on either side.
--- partB-question.hs
+++ partB-question.hs
@@ -3,12 +3,15 @@
 -- showT (runTM (tmPal (Tape blanks 'a' ("bbb" ++ blanks))))
 -- where a is the first char on the tape and bbb is the rest of the tape. if only 1 char use (Tape blanks 'a' blanks)
 
+
 --Representation of input tape for a Turing Machine
 data Tape a = Tape [a] a [a]
 
---Operations to move left or right and write on the tape 
+--Operations to move left or right on the tape
 moveLeft  (Tape (x:xs) y zs) = Tape xs x (y:zs)
 moveRight (Tape xs y (z:zs)) = Tape (y:xs) z zs
+
+--Operation to write the symbol on the tape
 write (Tape xs y zs) a = Tape xs a zs
 
 data TM a s = TM [a] 
@@ -26,37 +29,40 @@ runTM (TM as ss s0 t0 tf) =
 showT (Tape xs y zs) = y
 
 blanks = repeat ' '
-
---implementation of the Turing machinie
+--implementation of the Turing machine
 palin:: Int -> Tape Char -> Maybe(Int, Tape Char)
 palin s t@(Tape _ a _) = 
    case (s,a) of
       (0,'a')  -> Just (10, moveRight (write t ' '))  -- first item a
       (0,'b')  -> Just (20, moveRight (write t ' '))  -- First item b
-      (0,' ')  -> Just (30, write t 'Y')  --nothin
+      (0,' ')  -> Just (30, write t 'a')  --nothin
       (10,'a') -> Just (10, moveRight t) 
       (10,'b') -> Just (10, moveRight t)
       (10,' ') -> Just (11, moveLeft t)
       (11,'a') -> Just (12, moveLeft (write t ' '))
-      (11,'b') -> Just (30, write t 'N')   -- start a end b reject
-      (11,' ') -> Just (30, write t 'Y')   -- only a accept
-      (12,'a') -> Just (12, moveLeft t)
-      (12,'b') -> Just (12, moveLeft t)
+      (11,'b') -> Just (30, write t 'b')   -- start a end b reject
+      (11,' ') -> Just (30, write t 'a')   -- only a accept
+      (12,'a') -> Just (15, moveLeft (write t ' '))
+      (12,'b') -> Just (16, moveLeft (write t ' '))
       (12,' ') -> Just (0, moveRight t)
+      (15,'a') -> Just (30, write t 'a')
+      (15,'b') -> Just (30, write t 'b')
+      (15,' ') -> Just (30, write t 'a')
+      (16,'a') -> Just (30, write t 'b')
+      (16,'b') -> Just (30, write t 'a')
+      (16,' ') -> Just (30, write t 'a')
       (20,'a') -> Just (20, moveRight t)
       (20,'b') -> Just (20, moveRight t)
       (20,' ') -> Just (21, moveLeft t)
       (21,'b') -> Just (22, moveLeft (write t ' '))
-      (21,'a') -> Just (30, write t 'N')   -- start a end b reject
-      (21,' ') -> Just (30, write t 'Y')   -- only a accept
-      (22,'b') -> Just (22, moveLeft t)
-      (22,'a') -> Just (22, moveLeft t)
+      (21,'a') -> Just (30, write t 'b')   -- start a end b reject
+      (21,' ') -> Just (30, write t 'a')   -- only a accept
+      (22,'b') -> Just (16, moveLeft (write t ' '))
+      (22,'a') -> Just (15, moveLeft (write t ' '))
       (22,' ') -> Just (0, moveRight t)
-      (30,'Y') -> Just (30, write t 'a')
-      (30,'N') -> Just (30, write t 'b')
       (30,'a') -> Nothing
       (30,'b') -> Nothing
       _        -> error (show s ++ show a)
 
 tmPal :: Tape Char -> TM Char Int
-tmPal t = TM " abYN" [0,10,11,12,20,21,22,30] 0 t palin
+tmPal t = TM " ab" [0,10,11,12,15,16,20,21,22,30] 0 t palin
